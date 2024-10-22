b1df14b595eb8f61b06506fbaf2ab940b9e1bdd8
madc0der <oleg.vashenkov@gmail.com>
2024-10-23 00:11:59 +0300

    Initial commit - FixedTwistCorrection constraint

---
+++ Editor/AnimationRig/Unhide/FoldoutState.cs
@@ -0,0 +1,42 @@
+using UnityEditor;
+
+namespace AnimationRiggedExtensionsEditor.AnimationRig.Constraints
+{
+    class FoldoutState
+    {
+        bool m_Initialized;
+        bool m_Value;
+        string m_Name;
+
+        public bool value
+        {
+            get
+            {
+                if (!m_Initialized)
+                {
+                    m_Value = EditorPrefs.GetBool(m_Name, m_Value);
+                    m_Initialized = true;
+                }
+                return m_Value;
+            }
+            set
+            {
+                if (m_Value != value)
+                    EditorPrefs.SetBool(m_Name, m_Value = value);
+            }
+        }
+
+        FoldoutState() {}
+
+        public static FoldoutState Create<T>(string name, bool value) =>
+            new FoldoutState
+            {
+                m_Name = $"{typeof(T)}.{name}",
+                m_Value = value
+            };
+
+        public static FoldoutState ForSettings<T>() => Create<T>("Settings", false);
+
+        public static FoldoutState ForSourceObjects<T>() => Create<T>("SourceObjects", true);
+    }
+}
